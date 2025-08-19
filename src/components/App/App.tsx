fe804f1d55a747dae70e425338d097ac9f416b40
Romana Solovan <solo.rv95@gmail.com>
2025-08-19 15:36:21 +0300

    complete tasks functions

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,15 +3,21 @@ import "react-tabs/style/react-tabs.css";
 import FlightFeedback from "../../tabs/FlightFeedback/FlightFeedback";
 import Photos from "../../tabs/Photos/Photos";
 import PhotosTanstack from "../../tabs/PhotosTanstack/PhotosTanstack";
+import Tasks from "../../tabs/Tasks/Tasks";
 
 const App = () => {
   return (
     <Tabs>
       <TabList>
+      <Tab>Tasks</Tab>
         <Tab>PhotosTanstack</Tab>
         <Tab>Photos</Tab>
         <Tab>Flight Feedback</Tab>
       </TabList>
+
+      <TabPanel>
+        <Tasks/>
+      </TabPanel>
       <TabPanel>
         <PhotosTanstack />
       </TabPanel>
